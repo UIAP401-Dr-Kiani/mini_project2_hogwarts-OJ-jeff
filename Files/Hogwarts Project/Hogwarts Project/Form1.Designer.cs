872c337dc6ab5d37365c6cf1bc626ffd9e0bc5f2
OJ-jeff <Ojjeffries004>
2023-05-05 23:49:26 +0430

    Sign-in part has been written.

--- Files/Hogwarts Project/Hogwarts Project/Form1.Designer.cs
+++ Files/Hogwarts Project/Hogwarts Project/Form1.Designer.cs
@@ -33,66 +33,84 @@
             textBox2 = new TextBox();
             label2 = new Label();
             button1 = new Button();
+            textBox3 = new TextBox();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(262, 107);
+            textBox1.Location = new Point(295, 123);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(288, 27);
+            textBox1.Size = new Size(324, 30);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(181, 107);
+            label1.Location = new Point(204, 123);
             label1.Name = "label1";
-            label1.Size = new Size(75, 20);
+            label1.Size = new Size(79, 23);
             label1.TabIndex = 1;
             label1.Text = "Username";
             label1.Click += label1_Click;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(262, 204);
+            textBox2.Location = new Point(295, 234);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(288, 27);
+            textBox2.Size = new Size(324, 30);
             textBox2.TabIndex = 2;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(181, 211);
+            label2.Location = new Point(204, 243);
             label2.Name = "label2";
-            label2.Size = new Size(70, 20);
+            label2.Size = new Size(77, 23);
             label2.TabIndex = 3;
             label2.Text = "Password";
             label2.Click += label2_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(348, 275);
+            button1.Location = new Point(403, 305);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(106, 33);
             button1.TabIndex = 4;
             button1.Text = "Submit";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ActiveCaption;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Location = new Point(318, 38);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(286, 23);
+            textBox3.TabIndex = 6;
+            textBox3.Text = "WELCOME TO HOGWARTS SIMULATOR";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(9F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ActiveCaption;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(880, 391);
+            Controls.Add(textBox3);
             Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(textBox2);
             Controls.Add(label1);
             Controls.Add(textBox1);
+            Font = new Font("Nazanin", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Sign in";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -105,5 +123,6 @@
         private TextBox textBox2;
         private Label label2;
         private Button button1;
+        private TextBox textBox3;
     }
 }
--- Files/Hogwarts Project/Hogwarts Project/Form1.cs
+++ Files/Hogwarts Project/Hogwarts Project/Form1.cs
@@ -4,16 +4,16 @@ namespace Hogwarts_Project
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        Human[] Humans = new Human[400];
+        Jsonarray[] test = new Jsonarray[300];
+
+        string Jsonstring = File.ReadAllText("C:\\Users\\Amir\\source\\repos\\mini_project2_hogwarts-OJ-jeff\\Files\\JSON_DATA.json");
+        void PeopleSort()
         {
-            InitializeComponent();
-            Human[] Humans = new Human[400];
-            for (int i = 0; i < 400; ++i)
+            for (int i = 0; i < Humans.Length; i++)
             {
                 Humans[i] = new Human();
             }
-            string Jsonstring = File.ReadAllText("C:\\Users\\Amir\\source\\repos\\mini_project2_hogwarts-OJ-jeff\\Files\\JSON_DATA.json");
-            Jsonarray[] test = new Jsonarray[300];
             test = JsonConvert.DeserializeObject<Jsonarray[]>(Jsonstring);
             for (int i = 0; i < test.Length; ++i)
             {
@@ -47,17 +47,120 @@ namespace Hogwarts_Project
 
 
             }
+        }
 
-
+        public Form1()
+        {
+            InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+
+
         }
 
-       
 
-        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        int SignIn()
+        {
+            PeopleSort();
+            for (int i = 0; i < Humans.Length; ++i)
+            {
+                if (textBox1.Text == Humans[i].Username && textBox2.Text == Humans[i].Password && Humans[i].Role == Human.RoleType.Student)
+                {
+                    return 1;
+                }
+                else if (textBox1.Text == Humans[i].Username && textBox2.Text == Humans[i].Password && Humans[i].Role == Human.RoleType.Teacher)
+                {
+                    return 2;
+                }
+                else if (textBox1.Text == "ADMIN" && textBox2.Text == "ADMIN")
+                {
+                    return 3;
+                }
+
+            }
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Check = SignIn();
+
+                if (Check == 1)
+                {
+                    MessageBox.Show("Welcome Student!");
+                    this.Hide();
+                    var form2 = new Form2();
+                    form2.Closed += (s, args) => this.Close();
+                    form2.Show();
+                }
+                else if (Check == 2)
+                {
+
+                    MessageBox.Show("Welcome Teacher!");
+                    this.Hide();
+                    var form3 = new Form3();
+                    form3.Closed += (s, args) => this.Close();
+                    form3.Show();
+                }
+                else if (Check == 3)
+                {
+                    MessageBox.Show("Welcome Dumbeldore!");
+                    this.Hide();
+                    var form4 = new Form4();
+                    form4.Closed += (s, args) => this.Close();
+                    form4.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Please try again!");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERORR!!!");
+            }
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+                textBox3.Text = "WELCOME TO HOGWARTS SIMULATOR";
+                MessageBox.Show("ERORR!!!");
+            }
+            
+                
+        }
     }
 }
